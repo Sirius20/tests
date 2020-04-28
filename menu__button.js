f991a205ed55cebd022b22434002ceb9bf1ee1a4
Наталья <nataldenal89@gmail.com>
2020-04-28 17:53:43 +0500

    доделала кнопку меню

--- menu__button.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var button = document.querySelector('.menu__button');
-var menu = document.querySelector('.menu');
-var fieldset = document.querySelector('fieldset')
-
-button.onclick = function() {
-fieldset.setAttribute('disabled', 'disabled');
-}
